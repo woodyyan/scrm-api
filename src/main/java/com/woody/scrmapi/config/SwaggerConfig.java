400d5a1f5f664752d50faf3378d9185ec823ef75
edward_stan <2868984586@qq.com>
2022-06-02 18:23:56 +0800

    swagger配置

---
+++ src/main/java/com/woody/scrmapi/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.woody.scrmapi.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("box")
+                // 指定路径
+//                .pathsToMatch("/api/users/**")
+                // 指定特定的 API 文档信息
+                .addOpenApiCustomiser(userApiCustomise())
+                .build();
+    }
+
+    /**
+     * 定义 OpenApiCustomiser ，用于指定的 group
+     */
+    public OpenApiCustomiser userApiCustomise() {
+        return openApi -> openApi.info(new io.swagger.v3.oas.models.info.Info()
+                .title("SCRM Api文档")
+                .description("SCRM")
+                .version("1.0")
+                .contact(new io.swagger.v3.oas.models.info.Contact().name("Woody").email("woody@woody.com")));
+    }
+}
