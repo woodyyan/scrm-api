6554b0cfba217117b20937feb1a8867044adc566
Woody <colorguitar@hotmail.com>
2022-06-02 10:10:49 +0800

    first commit

---
+++ src/main/java/com/woody/scrmapi/ScrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.woody.scrmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woody/scrmapi/ScrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woody.scrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
